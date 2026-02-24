a1f2de631a46d2b3c07cb00c3b819c59577476d3
Adoraemonn <Ankit.davgandhinagar@gmail.com>
2026-02-24 21:50:59 +0530

    Sonar issue fixed

--- src/main/java/com/example/Backend/EMS/Employee/Management/System/controller/FrontendController.java
+++ src/main/java/com/example/Backend/EMS/Employee/Management/System/controller/FrontendController.java
@@ -2,12 +2,14 @@ package com.example.Backend.EMS.Employee.Management.System.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class FrontendController {
 
-    @RequestMapping(value = "/{path:[^\\.]*}")
+    @RequestMapping(value = "/{path:[^\\.]*}",method = RequestMethod.GET)
     public String forward() {
         return "forward:/index.html";
     }
 }
+
