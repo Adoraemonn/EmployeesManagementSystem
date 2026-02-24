7833a4e60959ebd8b314c62b71c19a6c9abc6c51
Adoraemonn <Ankit.davgandhinagar@gmail.com>
2026-02-24 23:42:31 +0530

    Sonar issue fixed

--- src/test/java/com/example/Backend/EMS/Employee/Management/System/EmployeeManagementSystemApplicationTests.java
+++ src/test/java/com/example/Backend/EMS/Employee/Management/System/EmployeeManagementSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.Backend.EMS.Employee.Management.System;
+package com.example.backend.ems.employee.management.system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
